f0ddfd84be6f656a25c2ab3d2f5dbb540664371d
_nnn_ <natchia.edu@gmail.com>
2025-04-24 17:50:48 +0800

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-source ~/.zshrc
-echo "Ensure this is run at repository path"
-cd .
-pip install poetry
-poetry self add poetry-plugin-shell
-poetry shell
-poetry install
-pip install --editable .
